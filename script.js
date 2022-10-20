3fa61fc8b8f085c9fa46750af35589ca0107bb1a
Tyler Oates <oatesrotc548@gmail.com>
2022-10-19 18:43:34 -0700

    Sucessfully able to conver user input to a string

--- script.js
+++ script.js
@@ -5,19 +5,22 @@ var todayWind = document.querySelector('#today-wind');
 var todayHumidity = document.querySelector('#today-humidity');
 var todayUvindex = document.querySelector('#today-uvindex');
 var searchCityForm = document.querySelector('#searchcityform');
-// var searchCityInput = document.querySelector('#searchcityinput');
+var searchCityInput = document.querySelector('#searchcityinput');
 
-
-
-// function searchFormInput() {
-//     const searchCityInput = document.getElementById("search").value;
-//     return searchCityInput;
-// }
-
-function appendSearch() {
-    var appendLocation = document.getElementById("search").value;
-    document.getElementById("location").innerHTML = appendLocation;
+// Converts the input value from the user searching to a string value.
+function searchFormSubmit() {
+    var searchCity = searchCityInput.value.trim();
+    fetchCityCoords(searchCity);
+    searchCity.value = '';
+    console.log(searchCity);
 }
+
+// Fetches the weather data from the (searchCity Parameter) 
+function fetchCityCoords(searchCity) {
+    var apiLink = 
+}
+
+searchCityForm.addEventListener('submit', searchFormSubmit);
 
 
 
