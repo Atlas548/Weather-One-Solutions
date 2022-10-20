0c30939d74caaa73d1d100b9ce360b5eb9502380
Tyler Oates <oatesrotc548@gmail.com>
2022-10-19 19:37:49 -0700

    Added fetchCityCoords & fetchWeather but 401 displ

--- script.js
+++ script.js
@@ -15,9 +15,34 @@ function searchFormSubmit() {
     console.log(searchCity);
 }
 
-// Fetches the weather data from the (searchCity Parameter) 
+// Fetches the weather location IE lat & Lon and is used as parameters for fetchWeather function
 function fetchCityCoords(searchCity) {
-    var apiLink = 
+    var apiLink = `https://api.openweathermap.org/geo/1.0/direct?q=${searchCity}&limit=5&appid=eecc46db8fa2385bac54dd8f8c273033`;
+    fetch(apiLink)
+        .then(function (res) {
+// Returns the json File
+            return res.json();
+        })
+// Send the data as a parameter to the fetchWeather function
+        .then(function (data) {
+            fetchWeather(data[0]);
+        })
+}
+
+// Uses the searched location from the fetchCityCoords function to fetch the data from the API
+function fetchWeather (location) {
+    var apiLink = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,hourly&appid=eecc46db8fa2385bac54dd8f8c273033`;
+    var city = location.name;
+    var {lat} = location;
+    var {lon} = location;
+
+    fetch(apiLink)
+    .then(function (res) {
+        return res.json();
+    })
+    .then(function (data) {
+        renderWeatherInfo(city, data)
+    })
 }
 
 searchCityForm.addEventListener('submit', searchFormSubmit);
